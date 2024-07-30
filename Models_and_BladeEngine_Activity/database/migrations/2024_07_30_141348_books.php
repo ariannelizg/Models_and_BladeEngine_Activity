f85c408a2a10a7d3604b19f3182ec0d2c934b602
ariannelizg <aig4561@students.uc-bcf.edu.ph>
2024-07-30 22:14:48 +0800

    Update: Generated and edited migration for the books table

---
+++ Models_and_BladeEngine_Activity/database/migrations/2024_07_30_141348_books.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('isbn')->unique();
+            $table->string('title');
+            $table->string('author');
+            $table->text('description')->nullable();
+            $table->date('date_published')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
